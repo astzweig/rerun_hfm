6b993a188c9dd7ed6913679569a99de2de21a81f
Ines Neubach <ines.neubach@idn.astzweig.de>
2015-02-01 00:26:53 +0100

    Add createFileByCpIfNotExists function, to create default host file from e.g. /etc/host

--- lib/baselib.sh
+++ lib/baselib.sh
@@ -62,3 +62,43 @@ function createDirIfNotExists {
     return 40;
   fi
 }
+
+function createFileByCpIfNotExists {
+  # Usage: createFileByCpIfNotExists <atPath> <fromPathSuggestion>
+  #
+  # Copies a file from a suggested path if user agrees and file
+  # does not exists already.
+  #
+  # @args:
+  #   str atPath: The path, where the file shall be copied to.
+  #   str fromPathSuggestion: A suggestion path, where the file shall be copied from.
+  #
+  # @version: 1.0
+  # @see: rerun_log
+  # @errors:
+  #   10: <fromPathSuggestion> is not a valid path to a file.
+  #   20: User disallowed using <fromPathSuggestion> as source for file.
+  #
+  if [ ! -f "${1}" ]; then
+    local USEDEFF;
+
+    if [ ! -f "${2}" ]; then
+      rerun_log debug "File at ${1} doesn't exist and suggested source file \
+      at ${2} does not exist either.";
+      return 10;
+    fi
+
+    read -p "No file found at ${1}, shall ${2} be copied there? (y/n)" USEDEFF;
+
+    if [[ ${USEDEFF} == y* || ${USEDEFF} == Y* ]]; then
+      rerun_log debug ">> mkdir \"${DEFAULT_FILE}\"";
+      cp "${2}" "${1}";
+    else
+      rerun_log debug "User disallowed coping ${2} to ${1}";
+      return 20;
+    fi
+  else
+    rerun_log debug "File ${1} already exists. All right then.";
+    return 0;
+  fi
+}
