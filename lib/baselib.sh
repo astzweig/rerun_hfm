a1d749a621cd3e3e770a864a09987f8b4481c5a7
Ines Neubach <ines.neubach@idn.astzweig.de>
2015-02-01 00:03:17 +0100

    Add file path setting constants

--- lib/baselib.sh
+++ lib/baselib.sh
@@ -4,3 +4,9 @@
 # Author: Ines Neubach <ines.neubach@idn.astzweig.de>
 #
 
+# - - -
+# Constants declared here.
+# - - -
+HOST_FILE_LOC="/etc/host";
+HFM_DIR="${HOME}/.hfm";
+DEFAULT_FILE="$HFM_DIR/default";
