d0939f442cc871d507d9b4159eb35064c67ac272
Ines Neubach <ines.neubach@idn.astzweig.de>
2015-02-09 14:05:28 +0100

    Test for baselib - createFileByCpIfNotExists. Includes basic skeleton

---
+++ tests/baselib-createFileByCpIfNotExists-1-test.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env roundup
+#
+# Baselib test
+#/ usage: rerun stubbs:test -m MODULE -p baselib
+#
+# Author: Ines Neubach <ines.neubach@idn.astzweig.de>
+#
+
+# Include baselib file
+# -----------------
+MODULE="$(basename $(cd ..; pwd))";
+BASELIB_PATH="${RERUN_MODULES}/${MODULE}/lib/baselib.sh";
+if [ ! -f ${BASELIB_PATH} ]; then
+    exit;
+fi
+
+source ${BASELIB_PATH};
