669f5237ad4003f0b886aea2adad3bb1614d0e7e
Ines Neubach <ines.neubach@idn.astzweig.de>
2015-02-09 15:34:20 +0100

    Add semicola, join traps together, remove unneccessary traps - all from test file baselib - createFileByCpIfNotExists

--- tests/baselib-createFileByCpIfNotExists-1-test.sh
+++ tests/baselib-createFileByCpIfNotExists-1-test.sh
@@ -29,7 +29,7 @@ it_stops_with_no_arguments() {
 
 it_stops_with_source_being_empty_or_invalid() {
   local RETV DEST_FN="${MODULE}-notexistingfile1-$$.txt";
-  local SRC_FN="${MODULE}-notexistingfile2-$$.txt"
+  local SRC_FN="${MODULE}-notexistingfile2-$$.txt";
   test ! -f "${DEST_FN}";
   test ! -f "${SRC_FN}";
 
@@ -48,8 +48,7 @@ it_stops_with_user_disallowing_source_creation() {
   local RETV DEST_FN="${MODULE}-notexistingfile1-$$.txt";
   local SRC_FN="${MODULE}-existingfile1-$$.txt";
   [ ! -f "${SRC_FN}" ] && touch "${SRC_FN}";
-  trap "rm -f \"${SRC_FN}\"" EXIT INT;
-  trap "rm -f \"${DEST_FN}\"" EXIT INT;
+  trap "rm -f ${SRC_FN}" EXIT INT;
   test ! -f "${DEST_FN}";
   test -f "${SRC_FN}";
 
@@ -61,9 +60,9 @@ it_stops_with_user_disallowing_source_creation() {
 
 it_works_with_source_already_existing() {
   local RETV DEST_FN="${MODULE}-existingfile1-$$.txt";
-  local SRC_FN="${MODULE}-notexistingfile1-$$.txt"
+  local SRC_FN="${MODULE}-notexistingfile1-$$.txt";
   [ ! -f "${DEST_FN}" ] && touch "${DEST_FN}";
-  trap "rm -f \"${DEST_FN}\"" EXIT INT;
+  trap "rm -f ${DEST_FN}" EXIT INT;
 
   test -f "${DEST_FN}";
   test ! -f "${SRC_FN}";
@@ -75,16 +74,15 @@ it_works_with_source_already_existing() {
 
 it_works_with_user_allowing_source_creation() {
   local RETV DEST_FN="${MODULE}-notexistingfile1-$$.txt";
-  local SRC_FN="${MODULE}-existingfile1-$$.txt" TESTSTR="Hello-World-$$";
+  local SRC_FN="${MODULE}-existingfile12-$$.txt" TESTSTR="Hello-World-$$";
   [ ! -f "${SRC_FN}" ] && echo "${TESTSTR}" >> "${SRC_FN}";
-  trap "rm -f ${DEST_FN}" EXIT INT;
-  trap "rm -f ${SRC_FN}" EXIT INT;
+  trap "{ rm -f ${SRC_FN}; rm -f ${DEST_FN}; }" EXIT INT;
   test ! -f "${DEST_FN}";
   test -f "${SRC_FN}";
 
   RETV="$(createFileByCpIfNotExists ${DEST_FN} ${SRC_FN} <<< "yes" && echo $? || echo $?)";
   test ${RETV} -eq 0;
   test -f "${DEST_FN}";
-  test "$(cat ${DEST_FN})" = "${TESTSTR}";
+  test "$(cat ${DEST_FN})" == "${TESTSTR}";
   test -f "${SRC_FN}";
 }
