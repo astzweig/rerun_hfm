45cba03c7b9f46212a6b1290e2a8f94a8aeca44d
Ines Neubach <ines.neubach@idn.astzweig.de>
2015-02-09 14:49:05 +0100

    Add test 'it_works_with_source_already_existing' for baselib - createFileByCpIfNotExists. Remove quotes around arguments

--- tests/baselib-createFileByCpIfNotExists-1-test.sh
+++ tests/baselib-createFileByCpIfNotExists-1-test.sh
@@ -33,13 +33,27 @@ it_stops_with_source_being_empty_or_invalid() {
   test ! -f "${DEST_FN}";
   test ! -f "${SRC_FN}";
 
-  RETV="$(createFileByCpIfNotExists \"${DEST_FN}\" \"\" && echo $? || echo $?)";
+  RETV="$(createFileByCpIfNotExists ${DEST_FN} \"\" && echo $? || echo $?)";
   test ${RETV} -eq 20;
   test ! -f "${DEST_FN}";
   test ! -f "${SRC_FN}";
 
-  RETV="$(createFileByCpIfNotExists \"${DEST_FN}\" \"${SRC_FN}\" && echo $? || echo $?)";
+  RETV="$(createFileByCpIfNotExists ${DEST_FN} ${SRC_FN} && echo $? || echo $?)";
   test ${RETV} -eq 20;
   test ! -f "${DEST_FN}";
+  test ! -f "${SRC_FN}";
+}
+
+it_works_with_source_already_existing() {
+  local RETV DEST_FN="${MODULE}-existingfile1-$$.txt";
+  local SRC_FN="${MODULE}-notexistingfile1-$$.txt"
+  [ ! -f "${DEST_FN}" ] && touch "${DEST_FN}";
+  trap "rm -f \"${DEST_FN}\"" EXIT INT;
+
+  test -f "${DEST_FN}";
+  test ! -f "${SRC_FN}";
+  RETV="$(createFileByCpIfNotExists ${DEST_FN} ${SRC_FN} && echo $? || echo $?)";
+  test ${RETV} -eq 0;
+  test -f "${DEST_FN}";
   test ! -f "${SRC_FN}";
 }
