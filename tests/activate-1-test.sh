28f893c9a661292e08c7b8f7d6d39e2a1be9f052
Thomas Stahler <thomas.stahler@idn.astzweig.de>
2015-02-15 01:15:51 +0100

    Generate new command 'activate' with 'environment' option

---
+++ tests/activate-1-test.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env roundup
+#
+#/ usage:  rerun stubbs:test -m hfm -p activate [--answers <>]
+#
+
+# Helpers
+# -------
+[[ -f ./functions.sh ]] && . ./functions.sh
+
+# The Plan
+# --------
+describe "activate"
+
+# ------------------------------
+# Replace this test. 
+it_fails_without_a_real_test() {
+    exit 1
+}
+# ------------------------------
+
