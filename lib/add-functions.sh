32dfbe41160b72b8c9b4a3ef738ff18850843a50
Ines Neubach <ines.neubach@idn.astzweig.de>
2015-03-01 19:40:19 +0100

    Add functionality to 'addHostToFileIfNotExists' in 'lib/add-functions.sh'

--- lib/add-functions.sh
+++ lib/add-functions.sh
@@ -5,18 +5,12 @@
 # Author: Ines Neubach <ines.neubach@idn.astzweig.de>
 #
 
-# - - -
-# Constants declared here.
-# - - -
-DOMAINREGEX="^[a-zA-Z0-9]{1}[a-zA-Z0-9\.\-]+$";
-IPREGEX="^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$";
-
 
 # - - -
 # Functions declared here.
 # - - -
 function checkHostAndIPAddress {
-  # Usage: checkHostAndIPAddress host ipaddress
+  # Usage: checkHostAndIPAddress <host> <ipaddress>
   #
   # @args:
   #   str host:      One host or a list of hosts seperated by space
@@ -30,6 +24,8 @@ function checkHostAndIPAddress {
   #   10: Invalid host given
   #   20: Invalid IP address given.
   #
+  local DOMAINREGEX="^[a-zA-Z0-9]{1}[a-zA-Z0-9\.\-]+$";
+  local IPREGEX="^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$";
   rerun_log info "Entering checkHostAndIPAddress function with $# arguments";
   if [[ ! "$1" =~ $DOMAINREGEX ]]; then
     rerun_log info ">> Invalid host name, returning";
@@ -43,17 +39,31 @@ function checkHostAndIPAddress {
 }
 
 function addHostToFileIfNotExists {
-  # Usage: addHostToFileIfNotExists <filePath> <address> <hosts>
+  # Usage: addHostToFileIfNotExists <filePath> <host> <ipaddress>
   #
   # Adds an host with its address to specified file.
   #
   # @args:
-  #   str filePath: The path to the (host)file, where the host shall be added
-  #   str address:  The ip address of the host
+  #   str filePath:  The path to the (host)file, where the host shall be added
+  #   str ipaddress: The ip address of the host
   #
   # @version: 1.0
-  # @see: rerun_log
+  # @see: rerun_log, checkHostAndIPAddress
   # @examples:
-  #   addHostToFileIfNotExists /etc/hosts 127.0.0.1 "newhost.dev www.newhost.dev"
+  #   addHostToFileIfNotExists /etc/hosts "newhost.dev www.newhost.dev" 127.0.0.1
+  # @errors:
+  #   10-20: see checkHostAndIPAddress
+  #   30: Invalid host file path given
+  #   40: Invalid IP address given.
   #
+  rerun_log info "Entering addHostToFileIfNotExists function with $# arguments";
+  [ ! -f "$1" ] && return 30;
+  checkHostAndIPAddress "$2" "$3";
+  if [ $? -ne 0 ]; then
+    rerun_log ">> Invalid arguments provided, returning";
+    return $?;
+  fi
+
+  echo -e "${3}\t${2}" >> "${1}"
+  return 0;
 }
