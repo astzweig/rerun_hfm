55e2553d9255ef57bad306cbe10fdf1fc4ff75a2
Ines Neubach <ines.neubach@idn.astzweig.de>
2015-03-01 13:13:57 +0100

    Add function 'checkHostAndIPAddress' to 'lib/add-functions.sh'

--- lib/add-functions.sh
+++ lib/add-functions.sh
@@ -6,8 +6,42 @@
 #
 
 # - - -
+# Constants declared here.
+# - - -
+DOMAINREGEX="^[a-zA-Z0-9]{1}[a-zA-Z0-9\.\-]+$";
+IPREGEX="^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$";
+
+
+# - - -
 # Functions declared here.
 # - - -
+function checkHostAndIPAddress {
+  # Usage: checkHostAndIPAddress host ipaddress
+  #
+  # @args:
+  #   str host:      One host or a list of hosts seperated by space
+  #   str ipaddress: The IP address where the host(s) shall be mapped to
+  #
+  # @version: 1.0
+  # @see: rerun_log
+  # @examples:
+  #   checkHostAndIPAddress "newhost.dev www.newhost.dev" 127.0.0.1
+  # @errors:
+  #   10: Invalid host given
+  #   20: Invalid IP address given.
+  #
+  rerun_log info "Entering checkHostAndIPAddress function with $# arguments";
+  if [[ ! "$1" =~ $DOMAINREGEX ]]; then
+    rerun_log info ">> Invalid host name, returning";
+    return 10;
+  fi
+  if [[ ! $IP =~ $IPREGEX ]]; then
+    rerun_log info ">> Invalid ip address, returning";
+    return 20;
+  fi
+  return 0;
+}
+
 function addHostToFileIfNotExists {
   # Usage: addHostToFileIfNotExists <filePath> <address> <hosts>
   #
